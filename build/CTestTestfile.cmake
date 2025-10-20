a48657a4bc4f8e36c74451391f51e1a9cf15de8d
coffeepudding <wangyifan785623@outlook.com>
2025-10-20 08:11:28 -0700

    delete

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/coffee/catkin_ws4/src
-# Build directory: /home/coffee/catkin_ws4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("tcp_receive_pkg")
-subdirs("slam_nav_pkg")
--- build/tcp_receive_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/coffee/catkin_ws4/src/tcp_receive_pkg
-# Build directory: /home/coffee/catkin_ws4/build/tcp_receive_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
